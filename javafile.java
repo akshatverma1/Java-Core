38a5508eee617af727816db1749d6babb81f31e2
akshatverma1 <akshaverma14@gmail.com>
2023-12-27 21:26:03 +0530

    27/12/2023

---
+++ javafile.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class javafile{
+    public static void main(String args[]){
+        // System.out.println("hello world%");
+        // System.out.println("akshat");
+        // System.out.print("hello world");
+        Scanner ak = new Scanner(System.in);
+        String name = ak.nextLine();
+        System.out.println(name);
+    }
+}
