be6a87409993fb58e215566733608cc6ce8d7c28
akshatverma1 <akshaverma14@gmail.com>
2024-01-08 21:20:28 +0530

    08/01/2024

---
+++ ass_11.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ass_11 {
+public static void main(String a[]) {
+    Scanner ak = new Scanner(System.in);
+    System.out.println("Enter the Any Integer");
+    int n = ak.nextInt();
+        char u ='a';
+        for(int i=1;i<=n;i++){
+            for(int j = 1;j<=i;j++)
+            {
+            System.out.print(u);
+            u++;
+            }
+            System.out.println();
+        }
+    }
+}
