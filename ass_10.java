f74e28c483ea9ec75b051d0ddc1671ac0c302a94
akshatverma1 <akshaverma14@gmail.com>
2024-01-08 18:21:24 +0530

    08/01/2024

---
+++ ass_10.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ass_10 {
+    public static void main(String a[]){
+        Scanner am = new Scanner(System.in);
+        //int b = am.nextInt();
+        int e=1;
+        for (int i=1;i<=5;i++){
+            e=1;
+            for (int j=1;j<=i;j++){
+               //e=1; 
+               System.out.print(j);
+               e++;
+            }
+            System.out.println();
+        }     
+    }
+}
