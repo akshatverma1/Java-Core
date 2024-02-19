db9cb7ab01ee9fbaf3177925e5a1ed3590c61f30
akshatverma1 <akshaverma14@gmail.com>
2024-02-19 16:15:03 +0530

    19/02/2024

---
+++ halfpramid.java
@@ -0,0 +1,11 @@
+public class halfpramid {
+
+    public static void main(String[] a){
+        for(int i=1;i<=5;i++){
+            for(int j=5;j>=1;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
