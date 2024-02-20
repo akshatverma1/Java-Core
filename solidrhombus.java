6a2d3eb608ce58c42366dfb2bd82b0b386ceb40e
akshatverma1 <akshaverma14@gmail.com>
2024-02-20 19:24:58 +0530

    20/02/2024

---
+++ solidrhombus.java
@@ -0,0 +1,14 @@
+public class solidrhombus {
+    public static void main(String[] a){
+        for(int i=1;i<=4;i++){
+            for(int j=4;j>=i;j--){
+                System.out.print(" ");
+                }
+                for(int w=1;w<=4;w++){
+                    System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+    }
+}
