53bfa3bc11cb9c59197dc2cd3348a9afacdd344e
akshatverma1 <akshaverma14@gmail.com>
2024-02-21 13:29:40 +0530

    21/02/2024

---
+++ arrayinput.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class arrayinput {
+    public static void main(String[] a){
+        int ar[] = new int[100];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Any Number");
+        ar[56] = sc.nextInt();
+        System.out.println(ar[56]);
+    }
+}
