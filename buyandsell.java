7ae42414b7c5688abac8f90b91d03589736c4377
akshatverma1 <akshaverma14@gmail.com>
2024-03-12 01:18:30 +0530

    12/03/2024

---
+++ buyandsell.java
@@ -0,0 +1,31 @@
+public class buyandsell {
+    public static void stock(int prices[]){
+        int buying=prices[0];
+        int profit;
+        int max=0;
+        for (int i=0;i<prices.length;i++){
+            int selling=prices[i];
+            for(int j=i;j>=0;j--){
+                if (buying>=prices[j]) {
+                    buying=prices[j];
+                }
+               
+            }
+            //System.out.println(buying);
+            //System.out.println();
+            profit = (selling-buying);
+            
+            if(max<=profit){
+                max=profit;
+            }
+            buying=prices[0];
+        }   
+        //profit = Math.max(profit, buying);
+            System.out.println(max);     
+    }
+    
+    public static void main(String arr[]){
+        int prices[] = {7,1,5,3,6,4};    
+        stock(prices);
+    }
+}
