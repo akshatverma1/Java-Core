96c9609a46c4438086d6fed66f6846925c8c60be
akshatverma1 <akshaverma14@gmail.com>
2024-03-12 18:42:10 +0530

    12/03/2024

---
+++ Oop.java
@@ -0,0 +1,17 @@
+public class Oop {
+    public static void main(String[] args) {
+        bank a = new bank();
+        a.name="akshat";
+        a.mobile=9109;
+        
+        a.show();
+    }
+}
+class bank{
+        private int password;
+        public String name;
+        public int mobile;
+        void show(){
+        System.out.println(name+" "+mobile);
+    }
+}
---
+++ arrayq1.java
@@ -0,0 +1,15 @@
+//Given an integer array nums, return true 
+//if any value appears at least twice in the array, and return false if every element is distinct
+//Input:nums = [1, 2, 3, 1]
+//Output:   true
+public class arrayq1 {
+    public static void in(int nums[]){
+        for (int i=0;i<nums.length;i++){
+            System.out.println(nums[i]);
+        }
+    }
+    public static void main(String a[]){
+        int nums[]={1,2,3,1};
+        in(nums);        
+    }
+}
