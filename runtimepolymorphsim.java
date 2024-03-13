1948c1756da131e0c23b8541f3498ce3fd4634c4
akshatverma1 <akshaverma14@gmail.com>
2024-03-13 22:21:56 +0530

    13/03/2024

---
+++ runtimepolymorphsim.java
@@ -0,0 +1,17 @@
+public class runtimepolymorphsim {
+
+    public static void main(String[] args) {
+        runtime r = new runtime();
+        r.functionone(2);
+    }
+}
+
+class runtime {
+    void functionone(int x) {
+        System.out.println("interger type");
+    }
+
+    void functionone(char y) {
+        System.out.println("Charater type");
+    }
+}
