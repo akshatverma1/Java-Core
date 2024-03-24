7f645e695eda647931970e4dcd8e203e199bd72c
akshatverma1 <akshaverma14@gmail.com>
2024-03-24 15:41:29 +0530

    24/03/2024

---
+++ strings.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class strings {
+    public static void main(String arr[]) {
+        Scanner sc = new Scanner(System.in);
+        char matr[] = { 'a', 'b', 'c', 'd' };
+        String str = "abcd";
+        System.out.println(str);
+        String input;
+        input = sc.nextLine();
+        System.out.println(str.charAt(3));
+    }
+}
