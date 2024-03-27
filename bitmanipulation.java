d9601b800c12829c1d0797bab53e72d32512647b
akshatverma1 <akshaverma14@gmail.com>
2024-03-27 23:59:22 +0530

    22/3/2024

--- bitmanipulation.java
+++ bitmanipulation.java
@@ -2,6 +2,6 @@ import javax.swing.text.StyledEditorKit;
 
 public class bitmanipulation {
     public static void main(String arr[]) {
-        System.out.println(~0);
+        System.out.println(5 << 2);
     }
 }
