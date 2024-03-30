bc3480c81329187c2d94632dd04eee8f0973bbc0
akshatverma1 <akshaverma14@gmail.com>
2024-03-30 21:35:37 +0530

    30/03/2024

---
+++ keyrecursion.java
@@ -0,0 +1,13 @@
+public class keyrecursion {
+    public static int firstoccurance(int arr[], int key, int i) {
+        if (arr[i] == key) {
+            return i;
+        }
+        return firstoccurance(arr, key, i + 1);
+    }
+
+    public static void main(String ar[]) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        System.out.println(firstoccurance(arr, 3, 0));
+    }
+}
