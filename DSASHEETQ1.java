9e7d76605ef83dba02053f11d6e0bc30ea893acd
akshatverma1 <akshaverma14@gmail.com>
2024-04-08 15:17:01 +0530

    07/04/2024

--- DSASHEETQ1.java
+++ DSASHEETQ1.java
@@ -1,15 +1,22 @@
+//For a given integer array of size N.You have to find all the occurrences(indices)
+//of a given element(Key) and print them.
+//Use a recursive function to solve this problem.
 public class DSASHEETQ1 {
-    public static void keys(int arrr[]) {
-        for (int i = 0; i < arrr.length; i++) {
-            if (arrr[i] == 2) {
-                System.out.print(i + " ");
-            }
+    public static int keys(int arrr[], int u, int key) {
+        if (u < 0) {
+            return 1;
         }
+        if (arrr[u] == key) {
+            System.out.println(u);
+        }
+        keys(arrr, u - 1, key);
+        return 0;
     }
 
     public static void main(String arr[]) {
         int arrr[] = { 3, 2, 4, 5, 6, 2, 7, 2, 2 };
         int key = 2;
-        keys(arrr);
+        int y = arrr.length - 1;
+        keys(arrr, y, key);
     }
 }
