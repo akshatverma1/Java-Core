9d332b36916d078d64fdb55d1915c45929e323c4
akshatverma1 <akshaverma14@gmail.com>
2024-05-13 13:58:23 +0530

    13/05/2024

--- subset.java
+++ subset.java
@@ -1,5 +1,5 @@
 public class subset {
-    public static void subsett(String s, String empty, int i, int count) {
+    public static void subsett(String s, String empty, int i) {
         // base case
         if (i == s.length()) {
             System.out.println(empty);
@@ -7,13 +7,13 @@ public class subset {
             return;
         }
         // For Yes
-        subsett(s, empty + s.charAt(i), i + 1, count + 1);
+        subsett(s, empty + s.charAt(i), i + 1);
         // For No
-        subsett(s, empty, i + 1, count);
+        subsett(s, empty, i + 1);
 
     }
 
     public static void main(String a[]) {
-        subsett("abc", "", 0, 0);
+        subsett("abc", "", 0);
     }
 }
