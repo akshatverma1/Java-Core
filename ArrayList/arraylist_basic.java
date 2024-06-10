5b896ff14b13e59760326b1739617e5bc363b8d9
akshatverma1 <akshaverma14@gmail.com>
2024-06-11 03:55:20 +0700

    Array List: 1) Basic 2) ADD Element 3) Get Element 4) Remove Element 5) Set Value At Particular Index 6) Contain

--- ArrayList/arraylist_basic.java
+++ ArrayList/arraylist_basic.java
@@ -4,7 +4,30 @@ import java.util.*;
 
 public class arraylist_basic {
     public static void main(String arr[]) {
-        System.out.println("Akshat");
+        System.out.println("Akshatt");
         ArrayList<Integer> list = new ArrayList<>();
+
+        // Add Element
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list);
+
+        // get Element Value
+        System.out.println(list.get(2));
+
+        // Remove Element Value
+        list.remove(2);
+        System.out.println(list);
+
+        // set Value at Particular Index
+        list.set(0, 20);
+        System.out.println(list);
+
+        // Contain
+        System.out.println(list.contains(4));
+        System.out.println(list.contains(8));
     }
 }
