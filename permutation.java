b89526752d29505daf3306f1b642d18f4bd8bf41
akshatverma1 <akshaverma14@gmail.com>
2024-06-11 01:14:44 +0530

    11/06/2024

--- permutation.java
+++ permutation.java
@@ -1,10 +1,21 @@
 public class permutation {
-    public static void akshat() {
-        System.out.println("Akshat");
+    public static void combination(String newstr, String ans) {
+        // base case
+        if (0 == newstr.length()) {
+            System.out.println(ans);
+            return;
+        }
+        // Resurtion
+        for (int i = 0; i < newstr.length(); i++) {
+            char current = newstr.charAt(i);
+            // abcde = ab + de;
+            String delectstr = newstr.substring(0, i) + newstr.substring(i + 1);
+            combination(delectstr, ans + current);
+        }
     }
 
     public static void main(String arr[]) {
-        System.out.println("fddffffff");
-        System.out.println("Akshat Verma");
+        String str = "abc";
+        combination(str, "");
     }
 }
