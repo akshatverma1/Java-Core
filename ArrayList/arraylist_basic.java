d43ec368359823dcbe5db7482250cf78d708fb13
akshatverma1 <akshaverma14@gmail.com>
2024-06-11 02:06:24 +0530

    Array List: 1) Basic

---
+++ ArrayList/arraylist_basic.java
@@ -0,0 +1,10 @@
+package ArrayList;
+
+import java.util.*;
+
+public class arraylist_basic {
+    public static void main(String arr[]) {
+        System.out.println("Akshat");
+        ArrayList<Integer> list = new ArrayList<>();
+    }
+}
