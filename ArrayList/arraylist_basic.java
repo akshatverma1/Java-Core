c0c686dacf6b5c090bc4d57ca3d4736bf7697360
akshatverma1 <akshaverma14@gmail.com>
2024-07-03 22:24:31 +0530

    message

--- ArrayList/arraylist_basic.java
+++ ArrayList/arraylist_basic.java
@@ -18,6 +18,7 @@ public class arraylist_basic {
         list.add(100);
         list.add(2000);
         list.add(2000);
+        list.add(2000);
         list.add(1, 9);
         System.out.println(list);
 
