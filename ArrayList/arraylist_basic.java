9643f89eb85ebe05d585515013af7c76368daf79
akshatverma1 <akshaverma14@gmail.com>
2024-07-18 12:44:53 +0530

    16/07/2024

--- ArrayList/arraylist_basic.java
+++ ArrayList/arraylist_basic.java
@@ -6,8 +6,10 @@ public class arraylist_basic {
     public static void main(String arr[]) {
         System.out.println("Akshatt");
         ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> str = new ArrayList<>();
 
         // Add Element
+        str.add("Akshat is very Good boy");
         list.add(1);
         list.add(2);
         list.add(3);
@@ -15,20 +17,22 @@ public class arraylist_basic {
         list.add(5);
         list.add(90);
         list.add(213);
+        list.add(888);
         list.add(100);
         list.add(1, 9);
         System.out.println(list);
+        System.out.println(str);
 
-        // get Element Value
-        System.out.println(list.get(2));
+        // // get Element Value
+        // System.out.println(list.get(2));
 
-        // Remove Element Value
-        list.remove(2);
-        System.out.println(list);
+        // // Remove Element Value
+        // list.remove(2);
+        // System.out.println(list);
 
-        // set Value at Particular Index
-        list.set(0, 20);
-        System.out.println(list);
+        // // set Value at Particular Index
+        // list.set(0, 20);
+        // System.out.println(list);
 
         // Contain
         System.out.println(list.contains(4));
