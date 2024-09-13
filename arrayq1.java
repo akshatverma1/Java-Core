826ffa1d453484996d5ff148be93f0f8f58bbcbc
akshatverma1 <akshaverma14@gmail.com>
2024-09-14 00:04:59 +0530

    gdfg

--- arrayq1.java
+++ arrayq1.java
@@ -3,11 +3,13 @@
 //Input:nums = [1, 2, 3, 1]
 //Output:   true
 public class arrayq1 {
-    public static void in(int nums[]){
-        
+    public static void in(int nums[]) {
+
     }
-    public static void main(String a[]){
-        int nums[]={1,2,3,1};
-        in(nums);        
+
+    public static void main(String a[]) {
+        int nums[] = { 1, 2, 3, 1 };
+        in(nums);
+        System.out.println("akshat");
     }
 }
