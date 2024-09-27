f37030c63c1df2e0ab07acc9345169c46835f7c3
akshatverma1 <akshaverma14@gmail.com>
2024-09-27 21:58:21 +0530

    27/09/2024

--- arrayinput.java
+++ arrayinput.java
@@ -1,11 +1,16 @@
 import java.util.*;
+
 public class arrayinput {
-    public static void main(String[] a){
+    public static void main(String[] a) {
         int ar[] = new int[100];
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Any Number");
         ar[56] = sc.nextInt();
         System.out.println(ar[56]);
-        System.out.println("Length of Array "+ ar.length);
+        System.out.println("Akshat Verma");
+        System.out.println("Akshat Verma");
+        System.out.println("Akshat Verma");
+        System.out.println("Akshat Verma");
+        System.out.println("Length of Array " + ar.length);
     }
 }
