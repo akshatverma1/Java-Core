1fd26d043431c1d16514112bdf37d23d26921c3e
akshatverma1 <akshaverma14@gmail.com>
2024-10-01 01:39:49 +0530

    Indore

--- ass4.java
+++ ass4.java
@@ -2,6 +2,7 @@ public class ass4 {
     public static void main(String s[]) {
         int x = 5, y = 2;
         System.out.println("Indore");
+        System.err.println("Xylo");
         System.out.println(x);
     }
 }
