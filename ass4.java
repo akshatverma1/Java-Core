7c5c8b4601cc6509cdf819e274b828513b98f249
akshatverma1 <akshaverma14@gmail.com>
2024-10-01 01:40:30 +0530

    Message

--- ass4.java
+++ ass4.java
@@ -1,8 +1,7 @@
 public class ass4 {
     public static void main(String s[]) {
         int x = 5, y = 2;
-        System.out.println("Indore");
-        System.err.println("Xylo");
         System.out.println(x);
+        System.out.println(y);
     }
 }
