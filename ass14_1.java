2c05c47c2dc72cc0083502c4ab5ca2827e41467e
akshatverma1 <akshaverma14@gmail.com>
2024-10-02 13:44:36 +0530

    Javascript

--- ass14_1.java
+++ ass14_1.java
@@ -1,13 +1,17 @@
 import java.util.*;
+
 public class ass14_1 {
     Scanner ak = new Scanner(System.in);
-    public static int avg(int x,int y,int z){
+
+    public static int avg(int x, int y, int z) {
         int w;
-        w = (x+y+z)/3;
+        w = (x + y + z) / 3;
         return w;
     }
-    public static void main(String a[]){
-        int e = avg(1,2,3);
+
+    public static void main(String a[]) {
+        int e = avg(1, 2, 3);
         System.out.print(e);
+        System.out.println("React");
     }
 }
