3f715bf167b410bcf7c136a08c7229d41b417d32
akshatverma1 <akshaverma14@gmail.com>
2024-10-04 20:25:51 +0530

    react.js

--- newdate.java
+++ newdate.java
@@ -1,5 +1,8 @@
 public class newdate {
     public static void main() {
         System.out.println("AKSHAT");
+        for (int i = 10; i < 40; i++) {
+            System.out.println("Akshat");
+        }
     }
 }
