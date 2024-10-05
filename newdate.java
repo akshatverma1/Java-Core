07eeb841099454defd1b055935dd6426f9069cd5
akshatverma1 <akshaverma14@gmail.com>
2024-10-05 12:37:33 +0530

    messgae' ijl

--- newdate.java
+++ newdate.java
@@ -1,8 +1,6 @@
 public class newdate {
     public static void main() {
         System.out.println("AKSHAT");
-        for (int i = 10; i < 40; i++) {
-            System.out.println("Akshat");
-        }
+
     }
 }
