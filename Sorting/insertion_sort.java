8a6aac39eebbaa8684ab5ba1f8623ec538108bfc
akshatverma1 <akshaverma14@gmail.com>
2024-11-01 19:40:19 +0530

    Selection Sort

---
+++ Sorting/insertion_sort.java
@@ -0,0 +1,25 @@
+package Sorting;
+
+public class insertion_sort {
+    public static void main(String[] arr) {
+        int array[] = { 3, 5, 1, 7, 9, 2 };
+        int index = 0;
+        int min = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    index = j + 1;
+                }
+            }
+            for (int k = index; k < array.length; k++) {
+                for (int u = k; u < array.length; u++) {
+                    if (min < array[u]) {
+                        min = array[u];
+                    }
+                }
+            }
+
+        }
+        System.out.println(min);
+    }
+}
