3468f4e6ff22bab0e1ebef15747101c7ff505c16
akshatverma1 <akshaverma14@gmail.com>
2024-11-01 13:52:02 +0530

    message by 01/11/2024

---
+++ Sorting/bubble_sort.java
@@ -0,0 +1,7 @@
+package Sorting;
+
+public class bubble_sort {
+    public static void main(String[] aa) {
+        System.out.println("Bubble Sort");
+    }
+}
---
+++ Sorting/kadane_algorithms.java
@@ -0,0 +1,7 @@
+package Sorting;
+
+public class kadane_algorithms {
+    public static void main(String[] arr) {
+        System.out.println("Kadane Algorithms");
+    }
+}
