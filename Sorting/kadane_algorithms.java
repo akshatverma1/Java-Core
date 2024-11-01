07b76ebff791352b1d4200ffd23a56306f0ee9ff
akshatverma1 <akshaverma14@gmail.com>
2024-11-01 14:54:07 +0530

    message by 01/11/2024

--- Sorting/kadane_algorithms.java
+++ Sorting/kadane_algorithms.java
@@ -1,7 +1,26 @@
 package Sorting;
 
 public class kadane_algorithms {
+    // if a number is negative then so we take zero. ex: int q=-1 then q=0;
     public static void main(String[] arr) {
         System.out.println("Kadane Algorithms");
+
+        int arrayy[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+        int currentsum = 0;
+        int maxsum = 0;
+        for (int i = 0; i < arrayy.length; i++) {
+
+            currentsum = currentsum + arrayy[i];
+            if (currentsum < 0) {
+                currentsum = 0;
+            }
+            if (currentsum > maxsum) {
+                maxsum = currentsum;
+            }
+            System.out.print(arrayy[i] + ",");
+        }
+        System.out.println();
+        System.out.println(currentsum);
+        System.out.println(maxsum);
     }
 }
