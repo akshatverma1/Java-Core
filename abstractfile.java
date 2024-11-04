912883702c59a6d26dca15dbf53c6ddfa6543cba
akshatverma1 <akshaverma14@gmail.com>
2024-11-04 23:29:53 +0530

    03/11/2024

--- abstractfile.java
+++ abstractfile.java
@@ -29,7 +29,7 @@ class abstwo {
         System.out.println("Akshat verma");
         System.out.println("Akshat Verma");
         System.out.println("Akshat Verma");
-        System.out.println("Akshat Verma");
+
     }
 
     void date(int x) {
