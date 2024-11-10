358518e1188701603bc58df8ac7ed534b701ec08
akshatverma1 <akshaverma14@gmail.com>
2024-11-10 20:25:23 +0530

    Javascript

---
+++ practise.js
@@ -0,0 +1,14 @@
+let bj = {
+    akshat :{
+        name:"ajskaht verma ",
+    },
+    lakshya :{
+        name:"gggg",
+    },
+    pari :{
+        name:"pariii",
+    }
+}
+console.log(bj.akshat.name);
+let arrayy =["xylo","safari","santro","omni"];
+console.log(arrayy.indexOf("santro"));
