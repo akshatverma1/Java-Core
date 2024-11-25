35098fce62b8266a65c1cbd7f190b3160481c788
akshatverma1 <akshaverma14@gmail.com>
2024-11-25 22:32:25 +0530

    Push

---
+++ Question/anargram.java
@@ -0,0 +1,21 @@
+package Question;
+
+import java.util.*;
+
+public class anargram {
+    public static void main(String arr[]) {
+        String str1 = "lesson";
+        String str2 = "slient";
+        for (int i = 0; i < str1.length(); i++) {
+            for (int j = 0; j < str2.length(); j++) {
+                char s1 = str1.charAt(i);
+                char s2 = str2.charAt(j);
+                if (s1 == s2) {
+                    System.out.println("Anargram");
+                    break;
+                }
+            }
+            break;
+        }
+    }
+}
