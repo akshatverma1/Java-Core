e4220384b3a4be371431e40ae3300d9a395b092d
akshatverma1 <akshaverma14@gmail.com>
2024-11-25 20:48:07 +0530

    Push

---
+++ Ouestion/palindrome.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class palindrome {
+    public static void main(String arr[]) {
+        String str = "a man,a plant,a cannel";
+        for (int i = 0; i < str.length(); i++) {
+        }
+    }
+}
