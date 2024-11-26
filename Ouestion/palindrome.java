7c5fc99078a72adbeef61bfda4ebe2cc5cf28344
akshatverma1 <akshaverma14@gmail.com>
2024-11-26 19:44:21 +0530

    Push

--- Ouestion/palindrome.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class palindrome {
-    public static void main(String arr[]) {
-        String str = "a man,a plant,a cannel";
-        for (int i = 0; i < str.length(); i++) {
-        }
-    }
-}
--- akshat.java
+++ akshat.java
@@ -1,5 +1,27 @@
 public class akshat {
     public static void main(String[] args) {
+        String str = "lol";
+        String str1 = "";
+        String str2 = "";
+        for (int i = 0; i < str.length(); i++) {
+            char s = str.charAt(i);
+            if (!((s == ',') || (s == ' '))) {
+                str2 = str2 + Character.toString(s);
+            }
+        }
+        // System.out.println(str2);
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char s = str.charAt(i);
+            if (!((s == ',') || (s == ' '))) {
+                str1 = str1 + Character.toString(s);
+            }
+        }
+        // System.out.println(str1);
+        if (str1.equals(str2)) {
+            System.out.println("Palindome");
+        } else {
+            System.out.println("Do not Palindome");
+        }
 
     }
 }
