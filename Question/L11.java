75a5d3c730e67f18c1330211f107d81bf2231470
akshatverma1 <akshaverma14@gmail.com>
2024-12-03 20:31:56 +0530

    AKshat

--- Question/L11.java
+++ Question/L11.java
@@ -1,25 +1,28 @@
 package Question;
 
 public class L11 {
-    public static void main(String arr[]) {
+    public static int tappingwater() {
         int height[] = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
         int max = 0;
-        int count = 0;
+        int count = 1;
         for (int i = 0; i < height.length; i++) {
             for (int j = i + 1; j < height.length; j++) {
                 count = count + 1;
-                int originalHeight = 0;
+                int max1 = 0;
                 if (height[i] > height[j]) {
-                    originalHeight = height[j];
+                    max1 = count * height[j];
                 } else {
-                    originalHeight = height[i];
+                    max1 = count * height[i];
                 }
-                int max1 = count * originalHeight;
                 if (max1 > max) {
                     max = max1;
                 }
             }
         }
-        System.out.println(max);
+        return max;
+    }
+
+    public static void main(String arr[]) {
+        System.out.println(tappingwater());
     }
 }
