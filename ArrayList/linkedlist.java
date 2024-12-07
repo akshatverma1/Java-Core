83d9ea0e5a7489cf3c57dcaf33f2dfaa912212d6
akshatverma1 <akshaverma14@gmail.com>
2024-12-07 15:51:37 +0530

    Akshat

---
+++ ArrayList/linkedlist.java
@@ -0,0 +1,24 @@
+package ArrayList;
+
+import java.util.*;
+
+public class linkedlist {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node Head;
+    public static Node Tail;
+
+    public static void main(String arr[]) {
+        LinkedList l1 = new LinkedList();
+        System.out.println("akshat");
+    }
+
+}
