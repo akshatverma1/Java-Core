bf9b1215f29e2991b455e6cf63472e17687ec514
akshatverma1 <akshaverma14@gmail.com>
2024-12-17 23:50:03 +0530

    Link List

---
+++ ArrayList/linklist1.java
@@ -0,0 +1,11 @@
+package ArrayList;
+
+public class linklist1 {
+    class node1 {
+        String data;
+    }
+
+    public static void main(String arr[]) {
+
+    }
+}
--- ArrayList/list1.java
+++ ArrayList/list1.java
@@ -5,9 +5,10 @@ import java.util.*;
 public class list1 {
     public static void main(String arr[]) {
         int[] array = new int[2];
-        array[0] = 90;
         ArrayList<Integer> arraylist1 = new ArrayList<>();
+        arraylist1.add(1234);
+        arraylist1.add(8967);
+        arraylist1.add(4312);
         System.out.println(arraylist1);
-        System.out.println(array[0]);
     }
 }
