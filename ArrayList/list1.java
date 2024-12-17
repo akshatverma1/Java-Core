06106836c5136b55a29da9b8e03331b788efda9a
akshatverma1 <akshaverma14@gmail.com>
2024-12-17 20:53:17 +0530

    Link List

---
+++ ArrayList/list1.java
@@ -0,0 +1,13 @@
+package ArrayList;
+
+import java.util.*;
+
+public class list1 {
+    public static void main(String arr[]) {
+        int[] array = new int[2];
+        array[0] = 90;
+        ArrayList<Integer> arraylist1 = new ArrayList<>();
+        System.out.println(arraylist1);
+        System.out.println(array[0]);
+    }
+}
