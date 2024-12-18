73f2b0d2ee36f51af8e4b628728405d0f4388a47
akshatverma1 <akshaverma14@gmail.com>
2024-12-18 22:41:25 +0530

    Link List

--- ArrayList/linklist2.java
+++ ArrayList/linklist2.java
@@ -1,7 +1,25 @@
-package ArrayList;
-
 public class linklist2 {
-    public static void main(String arr[]) {
 
+    node head = null;
+
+    public class node {
+        String data;
+        node next;
+
+        node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void addone(String data) {
+
+        if (head == null) {
+
+        }
+    }
+
+    public static void main(String arr[]) {
+        linklist2 list = new linklist2();
     }
 }
