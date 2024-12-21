16b928547835d60f27b7bd6d9684ad27f91be17d
akshatverma1 <akshaverma14@gmail.com>
2024-12-21 15:00:16 +0530

    Akshat

--- LinkList/Linklist_Basic.java
+++ LinkList/Linklist_Basic.java
@@ -14,16 +14,51 @@ public class Linklist_Basic {
         }
     }
 
+    // Add in First
     public void addFirst(String data) {
         Node newNode = new Node(data);
         if (Head == null) {
             Head = newNode;
             return;
         }
+        newNode.next = Head;
+        Head = newNode;
+    }
 
+    // Add in Last
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (Head == null) {
+            Head = newNode;
+            return;
+        }
+        Node currentNode = Head;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
+    }
+
+    // print the linklist
+    public void PrintFunction() {
+        if (Head == null) {
+            System.out.println("Null Is Found");
+            return;
+        }
+        Node currentNode = Head;
+        while (currentNode != null) {
+            System.out.print(currentNode.data + "->");
+            currentNode = currentNode.next;
+        }
+        System.out.print("Null");
     }
 
     public static void main(String arr[]) {
         Linklist_Basic object1 = new Linklist_Basic();
+        // object1.addFirst("Akshat");
+        // object1.addLast("Verma");
+        // object1.addLast("Lakshya");
+        object1.PrintFunction();
     }
 }
