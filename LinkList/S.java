d384c360609d8cb98841f6acbde8caabb9631319
akshatverma1 <akshaverma14@gmail.com>
2024-12-23 19:23:33 +0530

    Akshat

---
+++ LinkList/S.java
@@ -0,0 +1,9 @@
+package LinkList;
+
+public class S {
+    public static void main(String arr[]) {
+        for (int i = 0; i < 50; i++) {
+            System.out.println("Sorry");
+        }
+    }
+}
