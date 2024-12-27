debdcd58af04a1fa3f32e1c761ec518c268f2b79
akshatverma1 <akshaverma14@gmail.com>
2024-12-27 21:02:54 +0530

    Akshat

--- LinkList/S.java
+++ LinkList/S.java
@@ -4,8 +4,10 @@ public class S {
     public static void main(String arr[]) {
         for (int i = 0; i < 50; i++) {
             System.out.println("Akshat");
+
             System.out.println("Akshat");
-            System.out.println("Akshat");
+
+            // System.out.println("Akshat");
         }
     }
 }
