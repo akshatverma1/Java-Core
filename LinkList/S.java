b0c3ece8990c48a0ae6f5e292837d1dc4de865be
akshatverma1 <akshaverma14@gmail.com>
2024-12-28 20:34:39 +0530

    Akshat

--- LinkList/S.java
+++ LinkList/S.java
@@ -3,7 +3,7 @@ package LinkList;
 public class S {
     public static void main(String arr[]) {
         for (int i = 0; i < 50; i++) {
-            // System.out.println("Akshat");
+            System.out.println("Akshat");
 
             // System.out.println("Akshat");
 
