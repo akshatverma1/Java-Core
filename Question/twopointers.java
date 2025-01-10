fe9ceea7e06fa517e279f5c31bb7127ba12bf6e3
akshatverma1 <akshaverma14@gmail.com>
2025-01-10 18:08:24 +0530

    Akshatverm a

---
+++ Question/twopointers.java
@@ -0,0 +1,27 @@
+class twopointers{
+    public static void main(String arr[]){
+        int array[] = {1,2,3,4,5,6,7,8};
+        System.out.println(twopointer(array));
+    }
+
+
+
+    public static int twopointer(int array[]){
+        int left = 0;
+        int right = array.length-1;
+        int target = 8;
+        while(left>right){
+            int summ = array[left]+array[right];
+            if(summ==target){
+                System.out.println("Akshat");
+                return target;
+            }
+            if(summ>target){
+                right++;
+            }else {
+                left++;
+            }
+        }
+        return 0;
+    }
+}
