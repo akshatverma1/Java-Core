81d377604e7bef994d4485d48109858a2a5256bf
akshatverma1 <akshaverma14@gmail.com>
2025-01-14 21:54:17 +0530

    Akshat

--- Question/maximum_sub_array.java
+++ Question/maximum_sub_array.java
@@ -1,5 +1,14 @@
 public class maximum_sub_array {
     public static void main(String arr[]) {
-        System.out.println("Akshat");
+        int arrayyy[] = { -1, 0, 1, 2, -1, -4 };
+        int width = 3;
+        int loopEnd = arrayyy.length - width;
+        int target = 10;
+        for (int i = 0; i < loopEnd - 1; i++) {
+            System.out.println("Outer");
+            for (int j = i; j < width; j++) {
+                System.out.println("Inner");
+            }
+        }
     }
 }
