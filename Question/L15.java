92bcc70004e003a2371ee50e56d954c6250d130e
akshatverma1 <akshaverma14@gmail.com>
2025-01-28 22:32:36 +0530

    Akshat Commit

--- Question/L15.java
+++ Question/L15.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 public class L15 {
     public static void main(String arr[]) {
-        // int arrayy[] = { -1, 0, 1, 2, -1, -4 };
+        int arrayy[] = { -1, 0, 1, 2, -1, -4 };
 
-        // ArrayList<String> arrr = new ArrayList<>();
+        ArrayList<String> arrr = new ArrayList<>();
 
-        // for (int i = 0; i < arrayy.length; i++) {
-        // for (int j = i + 1; j < arrayy.length; j++) {
-        // for (int k = j + 1; k < arrayy.length; k++) {
-        // if (arrayy[i] + arrayy[j] + arrayy[k] == 0) {
-        // System.out.println(arrayy[i] + " " + arrayy[j] + " " + arrayy[k]);
+        for (int i = 0; i < arrayy.length; i++) {
+            for (int j = i + 1; j < arrayy.length; j++) {
+                for (int k = j + 1; k < arrayy.length; k++) {
+                    if (arrayy[i] + arrayy[j] + arrayy[k] == 0) {
+                        System.out.println(arrayy[i] + " " + arrayy[j] + " " + arrayy[k]);
 
-        // arrr.add("[" + arrayy[i] + "," + arrayy[j] + "," + arrayy[k] + "]");
-        // }
-        // }
-        // }
-        // }
-        // System.out.println(arrr);
+                        // arrr.add("[" + arrayy[i] + "," + arrayy[j] + "," + arrayy[k] + "]");
+                    }
+                }
+            }
+        }
+        System.out.println(arrr);
         System.out.println(Core());
     }
 
