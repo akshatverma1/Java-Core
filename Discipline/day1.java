88a2c0d2475e010409317ca19f98c07275ee61ac
akshatverma1 <akshaverma14@gmail.com>
2025-04-10 23:55:19 +0530

    day1

---
+++ Discipline/day1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class day1 {
+    public static void main(String arr[]) {
+        int array[] = { 23, 54, 12, 33, 11, 2, 4, 6, 23 };
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+    }
+}
