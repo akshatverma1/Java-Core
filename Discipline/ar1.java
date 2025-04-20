b2fb0345787390f1ccca725f7d2f7cc0cdb6f09e
akshatverma1 <akshaverma14@gmail.com>
2025-04-20 15:20:12 +0530

    Message

--- Discipline/ar1.java
+++ Discipline/ar1.java
@@ -3,7 +3,6 @@ package Discipline;
 public class ar1 {
     public static void main(String arr[]) {
         int arry[] = { 3, 2, 3 };
-        int arlength = arry.length;
-
+        int arlength = arry.leng
     }
-}
+} 
