c76545acac9e7b714b0c780166292b7cb687742f
akshatverma1 <akshaverma14@gmail.com>
2025-04-24 23:26:25 +0530

    Java Added

--- Array/springmatrix.java
+++ Array/springmatrix.java
@@ -1,8 +1,8 @@
 package Array;
 
 public class springmatrix {
-    public static void main(String arrayy[]){
+    public static void main(String arrayy[]) {
         // int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-        while ()
+        // while ()
     }
 }
