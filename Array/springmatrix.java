2112b37bd9e5ea35f39dfdd83f68e7e60f8dfddd
akshatverma1 <akshaverma14@gmail.com>
2025-04-28 23:41:17 +0530

    Resume Shortlisted

--- Array/springmatrix.java
+++ Array/springmatrix.java
@@ -2,7 +2,7 @@ package Array;
 
 public class springmatrix {
     public static void main(String arrayy[]) {
-        // int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        int arr[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
         // while ()
     }
 }
