e8c1fe8045b872e1d49496ed3265c03ac318f7bc
akshatverma1 <akshaverma14@gmail.com>
2025-05-10 20:51:24 +0530

    Merge

--- Sorting/mergeSort.java
+++ Sorting/mergeSort.java
@@ -8,8 +8,53 @@ public class mergeSort {
         }
     }
 
+    public static void sort(int array[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+        int mid = si + (ei - si) / 2;
+        sort(array, si, mid);
+        sort(array, mid + 1, ei);
+        merge(array, si, ei, mid);
+    }
+
+    public static void merge(int array[], int si, int ei, int mid) {
+        int temp[] = new int[ei - si + 1];
+        int i = si;
+        int j = mid + 1;
+        int k = 0;
+
+        while ((i <= mid) && (j <= ei)) {
+            if (array[i] < array[j]) {
+                temp[k] = array[i];
+                i++;
+            } else {
+                temp[k] = array[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Left Side
+        while (i <= mid) {
+            temp[k++] = array[i++];
+        }
+
+        // right side
+        while (j <= ei) {
+            temp[k++] = array[j++];
+        }
+        // copy
+        int g = si, h = 0;
+
+        while (h < temp.length) {
+            array[g++] = temp[h++];
+        }
+    }
+
     public static void main(String arr[]) {
         int array[] = { 9, 2, 5, 7, 2, 4, 7, 1 };
+        sort(array, 0, array.length - 1);
         printArray(array);
     }
 }
