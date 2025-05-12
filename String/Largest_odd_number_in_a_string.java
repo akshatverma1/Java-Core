d7372655df5a00077133a8523a061261e43c6f12
akshatverma1 <akshaverma14@gmail.com>
2025-05-13 03:57:27 +0530

    reverse_words_in_a_string

--- String/Largest_odd_number_in_a_string.java
+++ String/Largest_odd_number_in_a_string.java
@@ -2,18 +2,35 @@ package String;
 
 public class Largest_odd_number_in_a_string {
     public static void main(String arr[]) {
-        String s = "34567";
+        String s = "34560";
         System.out.println(oddNumber(s));
     }
 
     public static String oddNumber(String s) {
         int number = Integer.parseInt(s);
+        StringBuilder strValue = new StringBuilder("");
+        int max = 0;
         if ((number % 2) != 0) {
-            String str = String.valueOf(number);
+            String str = Integer.toString(number);
             return str;
         } else {
+            for (int i = 0; i < s.length(); i++) {
+                for (int j = i; j < s.length(); j++) {
+                    for (int k = i; k < j; k++) {
+                        strValue.append(s.charAt(k));
+                    }
+                    String str2 = strValue.toString();
+                    int value = Integer.parseInt(str2);
+                    if ((value % 2) != 0) {
+                        if (value > max) {
+                            max = value;
+                        }
+                    }
+                }
 
+            }
         }
-        return "akash";
+        String Finalstr = Integer.toString(max);
+        return Finalstr;
     }
 }
