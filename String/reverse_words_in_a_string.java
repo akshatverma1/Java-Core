7dcf770fed6bf4ea068a835b0993606cdc348ba5
akshatverma1 <akshaverma14@gmail.com>
2025-05-13 02:21:52 +0530

    reverse_words_in_a_string

--- String/reverse_words_in_a_string.java
+++ String/reverse_words_in_a_string.java
@@ -12,8 +12,19 @@ public class reverse_words_in_a_string {
     public static void reverse(String str) {
         // System.out.println(str);
         StringBuilder strReverse = new StringBuilder(str);
+        StringBuilder finalstr = new StringBuilder("");
         strReverse.toString();
         strReverse.reverse();
-        System.out.println(strReverse);
+        for (int i = 0; i < strReverse.length(); i++) {
+            StringBuilder strWord = new StringBuilder();
+            strWord.toString();
+            while ((i < strReverse.length()) && ((strReverse.charAt(i)) != ' ')) {
+                strWord.append(strReverse.charAt(i));
+                i++;
+            }
+            strWord.reverse();
+            finalstr.append(" " + strWord);
+        }
+        System.out.println(finalstr.substring(1));
     }
 }
