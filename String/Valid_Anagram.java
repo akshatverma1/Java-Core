7e168c0557fd02db3df09c008addccc7a1a20f75
akshatverma1 <akshaverma14@gmail.com>
2025-05-16 03:27:17 +0530

    Valid Anagram

--- String/Valid_Anagram.java
+++ String/Valid_Anagram.java
@@ -4,31 +4,39 @@ import java.util.*;
 
 public class Valid_Anagram {
     public static void main(String arr[]) {
-        String s = "anagram", t = "nagaram";
+        String s = "car", t = "rac";
         System.out.println(Anagram(s, t));
     }
 
     public static boolean Anagram(String s, String t) {
-        int frequency[] = new int[128];
-        Arrays.fill(frequency, 0);
-        for (int i = 0; i < s.length(); i++) {
-            char f = s.charAt(i);
-            frequency[f]++;
+        int frequency1[] = new int[128];
+        Arrays.fill(frequency1, 0);
+        int frequency2[] = new int[128];
+        Arrays.fill(frequency2, 0);
+        if (s.length() != t.length()) {
+            return false;
         }
 
         for (int i = 0; i < s.length(); i++) {
+            char f = s.charAt(i);
+            frequency1[f]++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char f = t.charAt(i);
+            frequency2[f]++;
+        }
+        for (int i = 0; i < s.length(); i++) {
             for (int j = 0; j < s.length(); j++) {
-                if (s.charAt(i) == t.charAt(j)) {
-                    frequency[s.charAt(j)] -= 1;
+                if ((s.charAt(i) == s.charAt(j)) && (i != j)) {
+                    if (frequency1[s.charAt(i)] == frequency2[s.charAt(j)]) {
+                        break;
+                    } else {
+                        return false;
+                    }
                 }
             }
         }
 
-        for (int i = 0; i < s.length(); i++) {
-            if (frequency[s.charAt(i)] >= 0) {
-                return false;
-            }
-        }
         return true;
     }
 }
