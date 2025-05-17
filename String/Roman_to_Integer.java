71ad671a5a85d4061f7931ea68a3cc8ee62e5f86
akshatverma1 <akshaverma14@gmail.com>
2025-05-17 15:11:50 +0530

    Roman to Integer

--- String/Roman_to_Integer.java
+++ String/Roman_to_Integer.java
@@ -1,11 +1,30 @@
 package String;
 
+import java.util.*;
+
 public class Roman_to_Integer {
     public static void main(String arr[]) {
-
+        String s = "III";
+        System.out.println(roman(s));
     }
 
-    public static void roman(String s) {
-
+    public static int roman(String s) {
+        HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+        hm.put('I', 1);
+        hm.put('V', 5);
+        hm.put('X', 10);
+        hm.put('L', 50);
+        hm.put('C', 100);
+        hm.put('D', 500);
+        hm.put('M', 1000);
+        int result = hm.get(s.charAt(s.length() - 1));
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (hm.get(s.charAt(i)) >= hm.get(s.charAt(i + 1))) {
+                result += hm.get(s.charAt(i));
+            } else {
+                result -= hm.get(s.charAt(i));
+            }
+        }
+        return result;
     }
 }
