684d216084bdef7dea1c56d6e32b26b92b201133
akshatverma1 <akshaverma14@gmail.com>
2025-05-19 22:25:53 +0530

    Roman to integer

---
+++ String/String_to_Integer.java
@@ -0,0 +1,12 @@
+package String;
+
+public class String_to_Integer {
+    public static void main(String arr[]) {
+
+    }
+
+    public static int sToi(String s) {
+        System.out.println(s);
+        return 3;
+    }
+}
