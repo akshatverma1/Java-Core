76f4afc4bd97c46f547aae194d1e6ab4b8894827
akshatverma1 <akshaverma14@gmail.com>
2025-05-26 01:56:05 +0530

    Sum_of_Beauty_of_All_Substrings.java

---
+++ String/Sum_of_Beauty_of_All_Substrings.java
@@ -0,0 +1,38 @@
+package String;
+
+import java.util.*;
+
+public class Sum_of_Beauty_of_All_Substrings {
+    public static void main(String arr[]) {
+        String s = "aabcbaa";
+        System.out.println(Beauty(s));
+    }
+
+    public static int Beauty(String s) {
+        int beautyCount = 0;
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                String sub = s.substring(i, j + 1);
+                HashMap<Character, Integer> hm = new HashMap<>();
+                for (int k = 0; k < sub.length(); k++) {
+                    char c = sub.charAt(k);
+                    if (hm.containsKey(c)) {
+                        hm.put(c, hm.get(c) + 1);
+                    } else {
+                        hm.put(c, 1);
+                    }
+                }
+                int max = Integer.MIN_VALUE;
+                int min = Integer.MAX_VALUE;
+                for (int u = 0; u < sub.length(); u++) {
+                    max = Math.max(max, hm.get(sub.charAt(u)));
+                    min = Math.min(min, hm.get(sub.charAt(u)));
+                }
+                beautyCount = max - min;
+                sum += beautyCount;
+            }
+        }
+        return sum;
+    }
+}
