eb75a8c818ce95458e05f99b3c936224b8ed1de2
akshatverma1 <akshaverma14@gmail.com>
2025-05-26 03:03:26 +0530

    Sum_of_Beauty_of_All_Substrings.java

--- String/Sum_of_Beauty_of_All_Substrings.java
+++ String/Sum_of_Beauty_of_All_Substrings.java
@@ -36,3 +36,35 @@ public class Sum_of_Beauty_of_All_Substrings {
         return sum;
     }
 }
+
+// import java.util.*;
+
+// class Solution {
+// public int beautySum(String s) {
+// int beauty = 0;
+// int n = s.length();
+
+// for (int i = 0; i < n; i++) {
+// int[] freq = new int[26];
+
+// for (int j = i; j < n; j++) {
+// char c = s.charAt(j);
+// freq[c - 'a']++;
+
+// int max = 0;
+// int min = Integer.MAX_VALUE;
+
+// for (int f : freq) {
+// if (f > 0) {
+// max = Math.max(max, f);
+// min = Math.min(min, f);
+// }
+// }
+
+// beauty += (max - min);
+// }
+// }
+
+// return beauty;
+// }
+// }
