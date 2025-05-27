7514ffa57b1e004432fed592b994838ee3a66911
akshatverma1 <akshaverma14@gmail.com>
2025-05-27 23:50:14 +0530

    Update

---
+++ String/count_and_say.java
@@ -0,0 +1,18 @@
+package String;
+
+public class count_and_say {
+    public static void main(String arr[]) {
+        System.out.println(count(4));
+    }
+
+    public static String count(int n) {
+        int start = 0;
+        StringBuilder str = new StringBuilder("1");
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = 0; j < str.length(); j++) {
+
+            }
+        }
+        return str.toString();
+    }
+}
