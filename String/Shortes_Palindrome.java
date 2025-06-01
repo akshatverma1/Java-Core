6035efd4a030bdc6dfcccf1160215e7aca779f42
akshatverma1 <akshaverma14@gmail.com>
2025-06-02 02:15:48 +0530

    Short

--- String/Shortes_Palindrome.java
+++ String/Shortes_Palindrome.java
@@ -2,22 +2,25 @@ package String;
 
 public class Shortes_Palindrome {
     public static void main(String arr[]) {
-        String s = "abcd";
+        String s = "abbacd";
         System.out.println(shorts(s));
     }
 
     public static String shorts(String s) {
         StringBuilder str = new StringBuilder(s);
         int startIndex = 0;
-        int pivot = 0;
+        int pivot = 1;
         int endIndex = str.length() - 1;
         while (startIndex < endIndex) {
             if (str.charAt(startIndex) == str.charAt(endIndex)) {
                 startIndex++;
                 endIndex--;
+                System.out.println("apply");
             } else {
-                str.insert(0, str.charAt(pivot + 1));
+                System.out.println("Unapply");
+                str.reverse().append(s.charAt(pivot)).reverse();
                 pivot++;
+                endIndex++;
             }
         }
         return str.toString();
