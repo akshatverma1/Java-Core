4ced77f8f1be5bccd70f42c220cc80e07fbc36fa
akshatverma1 <akshaverma14@gmail.com>
2025-06-01 15:36:15 +0530

    Jva

--- String/count_and_say.java
+++ String/count_and_say.java
@@ -1,16 +1,33 @@
 package String;
 
 public class count_and_say {
-    public static void main(String arr[]) {
-        System.out.println(count(4));
+    public static void main(String[] args) {
+        System.out.println(count(4)); // Output: "1211"
     }
 
     public static String count(int n) {
-        int start = 0;
-        StringBuilder str = new StringBuilder("1");
-        for (int i = 0; i < str.length(); i++) {
-            // for (int j = 0; j < str.length(); j++) {
+        StringBuilder str1 = new StringBuilder("1");
 
+        for (int i = 2; i <= n; i++) {
+            StringBuilder str2 = new StringBuilder();
+            String s = str1.toString();
+
+            for (int j = 0; j < s.length();) {
+                int count = 1;
+
+                // Count how many times the current digit repeats
+                // for (int k = j + 1; k < s.length() && s.charAt(k) == s.charAt(j); k++) {
+                // count++;
+                // }
+
+                str2.append(count);
+                str2.append(s.charAt(j));
+                j += count; // move j to the next group
             }
-        }return str.toString();
-}}
+
+            str1 = str2;
+        }
+
+        return str1.toString();
+    }
+}
