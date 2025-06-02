e3a9c2a8e6cde561f3b3bb111f172c4e10a210f3
akshatverma1 <akshaverma14@gmail.com>
2025-06-02 15:29:18 +0530

    Rearrange

---
+++ String/Rearrange_Array_Elements_by_Sign.java
@@ -0,0 +1,12 @@
+package String;
+
+public class Rearrange_Array_Elements_by_Sign {
+    public static void main(String arr[]) {
+        int nums[] = { 3, 1, -2, -5, 2, -4 };
+        System.out.println(rearrange(nums));
+    }
+
+    public static int[] rearrange(int[] nums) {
+        return nums;
+    }
+}
