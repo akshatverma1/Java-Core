1014d8fe3c10a4ae6489cb430343da24cdf84667
akshatverma1 <akshaverma14@gmail.com>
2025-06-06 22:43:45 +0530

    Add

--- Array/Maximum_Product_Subarray.java
+++ Array/Maximum_Product_Subarray.java
@@ -7,17 +7,17 @@ public class Maximum_Product_Subarray {
     public static int maximum(int[] nums) {
         int product = 1;
         int u = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                for (int k = i; k <= j; k++) {
-                    product *= nums[k];
-                }
-                if (product > u) {
-                    u = product;
-                }
-                product = 1;
-            }
-        }
+        // for (int i = 0; i < nums.length; i++) {
+        // for (int j = i; j < nums.length; j++) {
+        // for (int k = i; k <= j; k++) {
+        // product *= nums[k];
+        // }
+        // if (product > u) {
+        // u = product;
+        // }
+        // product = 1;
+        // }
+        // }
         return u;
     }
 }
