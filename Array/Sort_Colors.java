edf568fbf5b91d38fe27e03ad0836c3b3221928c
akshatverma1 <akshaverma14@gmail.com>
2025-06-18 12:26:39 +0530

    Sort Color

--- Array/Sort_Colors.java
+++ Array/Sort_Colors.java
@@ -1,2 +1,22 @@
 public class Sort_Colors {
+    public static void main(String arr[]) {
+        int array[] = { 2, 0, 2, 1, 1, 0 };
+        sortColor(array);
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + " ");
+        }
+    }
+
+    public static int[] sortColor(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
 }
