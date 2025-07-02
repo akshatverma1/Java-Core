5cf742c4a9656d017d9f5082953df0c5e6e7888e
akshatverma1 <akshaverma14@gmail.com>
2025-07-02 21:46:37 +0530

    Nothing Lear

--- Array/merge_sort_array.java
+++ Array/merge_sort_array.java
@@ -1,13 +1,26 @@
+import java.util.Arrays;
+
 public class merge_sort_array {
     public static void main(String arr[]) {
         int a[] = { 1, 2, 3, 0, 0, 0 };
-        int b[] = { 0, 0, 0 };
+        int b[] = { 2, 5, 6 };
         int m = 3;
         int n = 3;
-        System.out.println(mergesort(a, b, m, n));
+        mergesort(a, b, m, n);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
     }
 
     public static int[] mergesort(int a[], int b[], int m, int n) {
+        if (m >= 0) {
+            int count = 0;
+            for (int i = m; i < m + n; i++) {
+                a[i] = b[count];
+                count++;
+            }
+            Arrays.sort(a);
+        }
         return a;
     }
 }
