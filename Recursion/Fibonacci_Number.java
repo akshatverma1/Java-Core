9ead1128a4386aaed02fa772f47684c69310ecc4
akshatverma1 <akshaverma14@gmail.com>
2025-07-09 23:52:03 +0530

    Fibonacci Number

--- Recursion/Fibonacci_Number.java
+++ Recursion/Fibonacci_Number.java
@@ -2,6 +2,13 @@ package Recursion;
 
 public class Fibonacci_Number {
     public static void main(String arr[]) {
+        System.out.println(Fibonacci(1));
+    }
 
+    public static int Fibonacci(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return Fibonacci(n - 2) + Fibonacci(n - 1);
     }
 }
