22392f0eedb6f6711c8da7984669ab226322c3bb
akshatverma1 <akshaverma14@gmail.com>
2025-07-16 15:19:48 +0530

    Search Insert Position

--- Binary_Search/Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
+++ Binary_Search/Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
@@ -3,40 +3,38 @@ public class Find_First_and_Last_Position_of_Element_in_Sorted_Array {
         int[] nums = { 5, 7, 7, 8, 8, 10 };
         int target = 8;
         int result[] = new int[2];
-        result[0] = searchRangeSecond(nums, target, 0, nums.length - 1);
-        result[1] = searchRangethird(nums, target, 0, nums.length - 1);
+        result[0] = searchRangeSecond(nums, target, 0, nums.length - 1, -1);
+        result[1] = searchRangethird(nums, target, 0, nums.length - 1, -1);
         System.out.print(result[0] + " " + result[1]);
     }
 
-    public static int searchRangeSecond(int[] nums, int target, int si, int ei) {
-        int ans = -1;
+    public static int searchRangeSecond(int[] nums, int target, int si, int ei, int ans0) {
         while (si <= ei) {
             int mid = si + (ei - si) / 2;
             if (nums[mid] == target) {
-                ans = mid;
+                ans0 = mid;
                 ei = mid - 1;
             } else if (target > nums[mid]) {
-                return searchRangeSecond(nums, target, mid + 1, ei);
+                return searchRangeSecond(nums, target, mid + 1, ei, ans0);
             } else {
-                return searchRangeSecond(nums, target, si, mid - 1);
+                return searchRangeSecond(nums, target, si, mid - 1, ans0);
             }
         }
-        return ans;
+        return ans0;
     }
 
-    public static int searchRangethird(int[] nums, int target, int si, int ei) {
-        int ans = -1;
+    public static int searchRangethird(int[] nums, int target, int si, int ei, int ans1) {
         while (si <= ei) {
             int mid = si + (ei - si) / 2;
             if (nums[mid] == target) {
-                ans = mid;
+                ans1 = mid;
                 si = mid + 1;
-            } else if (target > nums[mid]) {
-                return searchRangethird(nums, target, mid + 1, ei);
+            } else if (target >= nums[mid]) {
+                return searchRangethird(nums, target, mid + 1, ei, ans1);
             } else {
-                return searchRangethird(nums, target, si, mid - 1);
+                return searchRangethird(nums, target, si, mid - 1, ans1);
             }
         }
-        return ans;
+        return ans1;
     }
 }
