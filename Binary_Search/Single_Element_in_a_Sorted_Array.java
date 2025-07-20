90c01a58e9e22b662925e808321cb15156e67aee
akshatverma1 <akshaverma14@gmail.com>
2025-07-20 23:44:47 +0530

    Daily DSA

--- Binary_Search/Single_Element_in_a_Sorted_Array.java
+++ Binary_Search/Single_Element_in_a_Sorted_Array.java
@@ -11,15 +11,15 @@ public class Single_Element_in_a_Sorted_Array {
             return nums[0];
         }
 
-        // if (si <= ei) {
-        //     int mid = si + (ei - si) / 2;
-        //     if (mid == 0 && nums[mid] != nums[mid + 1])
-        //         return nums[mid];
-        //     if (mid == nums.length - 1 && nums[nums.length - 1] != nums[nums.length - 2])
-        //         return nums[mid];
-        //     if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
-        //         return nums[mid];
-        //     } else if (mid % 2 == 0) {
+        if (si <= ei) {
+            int mid = si + (ei - si) / 2;
+            if (mid == 0 && nums[mid] != nums[mid + 1])
+                return nums[mid];
+            if (mid == nums.length - 1 && nums[nums.length - 1] != nums[nums.length - 2])
+                return nums[mid];
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            } else if (mid % 2 == 0) {
                 if (nums[mid - 1] == nums[mid]) {
                     return single(nums, si, mid - 1);
                 } else {
@@ -32,4 +32,7 @@ public class Single_Element_in_a_Sorted_Array {
                     return single(nums, si, mid - 1);
                 }
             }
-        }return 1;}}
+        }
+        return 1;
+    }
+}
