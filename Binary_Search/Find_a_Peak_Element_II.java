af2ae04468e5e0b94b6875aa1ff5012df07b53a1
akshatverma1 <akshaverma14@gmail.com>
2025-07-25 23:41:57 +0530

    Akshat

--- Binary_Search/Find_a_Peak_Element_II.java
+++ Binary_Search/Find_a_Peak_Element_II.java
@@ -2,10 +2,4 @@ public class Find_a_Peak_Element_II {
     public static void main(String arr[]) {
 
     }
-
-    public int[] findPeakGrid(int[][] mat) {
-        int result[] = new int[2];
-
-        return result;
-    }
 }
