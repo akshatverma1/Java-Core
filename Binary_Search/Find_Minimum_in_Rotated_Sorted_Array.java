3a2879c9a0320759cb60cbe011660edc1f5e2a62
akshatverma1 <akshaverma14@gmail.com>
2025-07-29 14:02:27 +0530

    Find_Minimum_in_Rotated_Sorted_Array.java

---
+++ Binary_Search/Find_Minimum_in_Rotated_Sorted_Array.java
@@ -0,0 +1,11 @@
+public class Find_Minimum_in_Rotated_Sorted_Array {
+    public static void main(String arr[]) {
+        int[] nums = { 3, 4, 5, 1, 2 };
+        System.out.println(findMin(nums));
+    }
+
+    public static int findMin(int[] nums) {
+        int result = 0;
+        return result;
+    }
+}
