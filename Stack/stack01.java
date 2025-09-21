7949c7d1330d4cf30a3e60d5854702364cd3be5e
“akshatverma1 <akshaverma14@gmail.com>
2025-09-21 17:10:56 +0530

    stack

---
+++ Stack/stack01.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class stack01 {
+
+    static class stackOperation {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        public void push(int data) {
+            list.add(data);
+        }
+
+        public void pop() {
+            if (list.size() == 0) {
+                System.out.println("Stack is empty");
+                return;
+            }
+            list.remove(list.size() - 1);
+        }
+
+        public int peek() {
+            if (list.size() == 0) {
+                System.out.println("Stack is empty");
+                return -1;
+            }
+            return list.get(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] aa) {
+        System.out.println("Stack");
+        stackOperation s = new stackOperation();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        while (s.list.size() > 0) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+        System.out.println(s.peek());
+    }
+}
