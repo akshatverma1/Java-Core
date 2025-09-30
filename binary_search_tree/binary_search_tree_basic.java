398242bb2266246814d814548183923fb7a182a0
“akshatverma1 <akshaverma14@gmail.com>
2025-09-30 21:09:55 +0530

    Binary Search Tree

--- binary_search_tree/binary_search_tree_basic.java
+++ binary_search_tree/binary_search_tree_basic.java
@@ -23,11 +23,20 @@ public class binary_search_tree_basic {
         newNode.right = binary_tree(nodes);
         return newNode;
     }
+    public static void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
 }
     public static void main(String arr[]){
         tree t = new tree();
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         Node root = t.binary_tree(nodes);
         System.out.println(root.data);
+        t.preorder(root);
     }
 }
