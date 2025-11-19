2f4a1a25df04f1910650f918369988fd3d1d5002
“akshatverma1 <akshaverma14@gmail.com>
2025-11-19 15:04:32 +0530

    Cycle detection

--- Graph/cycle_detection.java
+++ Graph/cycle_detection.java
@@ -19,42 +19,29 @@ class cycle_detection{
         }
 
         //vertex = 0
+
         graph[0].add(new Edges(0, 1, 1));
-        graph[0].add(new Edges(0, 2, 1));
+        // graph[0].add(new Edges(0,2,1));
+        graph[0].add(new Edges(0,3,1));
 
-        //vertex = 1
+        //vertes = 1
 
-        graph[1].add(new Edges(1, 0, 1));
-        graph[1].add(new Edges(1,3,1));
+        graph[1].add(new Edges(1,0,1));
+        graph[1].add(new Edges(1,2,1));
 
-        //vertex = 2
+        //vertes = 2
 
-        graph[2].add(new Edges(2, 0, 1));
-        graph[2].add(new Edges(2, 4, 1));
+        // graph[2].add(new Edges(2, 0, 1));
+        graph[2].add(new Edges(2, 1, 1));
 
-        //vertex = 3 
+        //vertes = 3
 
-        graph[3].add(new Edges(3, 1, 1));
+        graph[3].add(new Edges(3, 0, 1));
         graph[3].add(new Edges(3, 4, 1));
-        graph[3].add(new Edges(3, 5, 1));
-
 
         //vertex = 4
 
-        graph[4].add(new Edges(4, 2, 1));
         graph[4].add(new Edges(4, 3, 1));
-        graph[4].add(new Edges(4, 5, 1));
-
-        //vertex = 5
-
-        graph[5].add(new Edges(5, 3, 1));
-        graph[5].add(new Edges(5, 4, 1));
-        graph[5].add(new Edges(5, 6, 1));
-
-
-        //vertex = 6
-        graph[6].add(new Edges(6, 5, 1));
-
 
     }
 
@@ -76,10 +63,53 @@ class cycle_detection{
             }
         }
     }
+
+    public static boolean cycle_detection_dfs_util(ArrayList <Edges> Graph[],int current,int parent,boolean visited[]){
+        System.out.println(current);
+        visited[current] = true;
+
+        for(int i=0;i<Graph[current].size();i++){
+            Edges e = Graph[current].get(i);
+
+            //case = 3
+            if(visited[e.Destition] == false){
+                if(cycle_detection_dfs_util(Graph, e.Destition, current, visited)){
+                    return true;
+                }
+            }
+
+            //case =2
+            //do Nothing
+
+
+            //case = 1
+
+            else if(visited[e.Destition]){
+                if(e.Destition != parent){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+    public static boolean cycle_detection_dfs(ArrayList<Edges> Graph[],int current){
+        boolean visited[] = new boolean[Graph.length];
+        for(int i=0;i<Graph.length;i++){
+            if(visited[i]==false){
+                if(cycle_detection_dfs_util(Graph,current,-1,visited)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     public static void main(String main[]){
-        int v=7;
+        int v=5;
         ArrayList <Edges> Graph[] = new ArrayList[v];
         creating_graph(Graph);
-        dfs(Graph, 0);
+        // dfs(Graph, 0);
+        
+        System.out.println(cycle_detection_dfs(Graph, 0));
     }
 }
